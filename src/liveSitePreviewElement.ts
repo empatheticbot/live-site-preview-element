22324465fabbd9d91eec152111a36add968f35d6
Steven Ledsworth <sledsworth@gmail.com>
2022-02-14 21:12:39 -0500

    started to play with clip path for a super-ellipse iframe

--- src/liveSitePreviewElement.ts
+++ src/liveSitePreviewElement.ts
@@ -3,8 +3,8 @@ export class LiveSitePreviewElement extends HTMLElement {
   url?: string
   description?: string
   heading?: string
-  height = 400
-  width = 200
+  height = 880
+  width = 398
 
   static get observedAttributes(): string[] {
     return ['url', 'title', 'description']
@@ -12,13 +12,29 @@ export class LiveSitePreviewElement extends HTMLElement {
 
   connectedCallback(): void {
     this.url = this.getAttribute('url') || ''
-    this.height = this.getAttribute('height')
+
     this.root = this.attachShadow({ mode: 'open' })
     const iframe = document.createElement('iframe')
+
+    const svg = document.createElement('svg')
+    // svg.setAttribute('height', '300')
+    // svg.setAttribute('width', '200')
+
+    svg.innerHTML = `
+      <defs>
+          <clipPath id="svgPath">
+              <path fill="#FFFFFF" stroke="#000000" stroke-width="1.5794" stroke-miterlimit="10" d="M215,100.3c97.8-32.6,90.5-71.9,336-77.6c92.4-2.1,98.1,81.6,121.8,116.4c101.7,149.9,53.5,155.9,14.7,178c-96.4,54.9,5.4,269-257,115.1c-57-33.5-203,46.3-263.7,20.1c-33.5-14.5-132.5-45.5-95-111.1C125.9,246.6,98.6,139.1,215,100.3z"/>
+          </clipPath>
+      </defs>
+   `
 
     iframe.src = this.url
     iframe.height = this.height.toString()
     iframe.width = this.width.toString()
+    iframe.style.overflow = 'hidden'
+    iframe.style.clipPath = 'url(#svgPath)'
+
+    this.root.append(svg)
     this.root.append(iframe)
   }
 
